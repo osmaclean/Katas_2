d28b82b3d069d884aa0ffece336261c43e1de0f6
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-11-22 12:25:05 -0300

    Primeira tarefa realizada com sucesso!

---
+++ script/katas2.js
@@ -0,0 +1,17 @@
+
+// Primeira Tarefa
+
+let resultado = []
+
+function add (number1, number2){
+    
+    resultado += number2 + number1
+    return resultado
+}
+console.log(add(2,4));
+
+// Segunda Tarefa
+
+
+
+
