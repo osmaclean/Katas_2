c1c0741a6118552a523a0479a591f8f1761c87ff
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-11-22 13:00:42 -0300

    Segunda tarefa realizada com sucesso!

--- script/katas2.js
+++ script/katas2.js
@@ -1,17 +1,24 @@
 
 // Primeira Tarefa
 
-let resultado = []
 
-function add (number1, number2){
-    
+
+function add(number1, number2) {
+    let resultado = 0;
     resultado += number2 + number1
-    return resultado
+    return resultado;
 }
-console.log(add(2,4));
+console.log(add(2, 4));
 
 // Segunda Tarefa
 
+function multiply(number3, number4) {
+    let result = 0;
+    for (let i = 0; i < number3; i++) {
+        result = add(result, number4);
+    }
+    return result;
+}
+console.log(multiply(6, 8));
 
-
-
+// 
